21777dee232d483a17b96279162a7479ee218ffc
Kareem Grant <kareem@getuserwise.com>
2013-06-27 11:46:00 -0600

    removed parameter validation logic

--- lib/message_fetcher.rb
+++ lib/message_fetcher.rb
@@ -7,12 +7,10 @@ module MessageFetcher
   extend self
 
   def for_tracks(track_id)
-    valid_track?(track_id)
     fetch_messages(track_id)
   end
 
   def create_message(data)
-    check_params(data)
     post_messages(data)
   end
 
@@ -46,28 +44,28 @@ private
     "http://localhost:3002"
   end
 
-  def valid_track?(track_id)
-    raise InvalidCredentials unless track_id.is_a?(Integer)
-  end
+  # def valid_track?(track_id)
+  #   raise InvalidCredentials unless track_id.is_a?(Integer)
+  # end
 
-  def check_params(params)
-    if all_params_present?(params)
-      validate_params(params)
-    else
-      raise InvalidCredentials
-    end
-  end
+  # def check_params(params)
+  #   if all_params_present?(params)
+  #     validate_params(params)
+  #   else
+  #     raise InvalidCredentials
+  #   end
+  # end
 
-  def all_params_present?(params)
-    params.has_key?(:track_id) && params.has_key?(:user_id) && params.has_key?(:body)
-  end
+  # def all_params_present?(params)
+  #   params.has_key?(:track_id) && params.has_key?(:user_id) && params.has_key?(:body)
+  # end
 
-  def validate_params(params)
-    raise InvalidCredentials unless params[:track_id].is_a?(Integer)
-    raise InvalidCredentials unless params[:user_id].is_a?(Integer)
-    raise InvalidCredentials unless !params[:body].empty?
-  end
+  # def validate_params(params)
+  #   raise InvalidCredentials unless params[:track_id].is_a?(Integer)
+  #   raise InvalidCredentials unless params[:user_id].is_a?(Integer)
+  #   raise InvalidCredentials unless !params[:body].empty?
+  # end
 
-end
+# end
 
-class InvalidCredentials < StandardError; end
+# class InvalidCredentials < StandardError; end
